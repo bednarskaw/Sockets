cf880b18142bf0a44f6b3b0546c9f23820960097
Weronika Bednarska <bednarska.w203@gmail.com>
2023-10-25 17:08:45 +0200

    Multiple Connections Java

--- SqTCPServ.java
+++ SqTCPServ.java
@@ -5,37 +5,63 @@ public class SqTCPServ {
 
     private static final int PORT = 5000;
 
-    private BufferedWriter w;
-    private BufferedReader r;
-    private Socket ns;
-    private ServerSocket s;
+    private ServerSocket serverSocket;
 
     public SqTCPServ() {
         try {
-            System.out.println("listening on port " + PORT);
-            s = new ServerSocket(PORT);
-            ns = s.accept();
-            s.close();
-            System.out.println("connection accepted");
-            r = new BufferedReader(new InputStreamReader(ns.getInputStream()));
-            w = new BufferedWriter(new OutputStreamWriter(ns.getOutputStream()));
-            char[] cbuf = new char[100];
-            String v = new String(cbuf, 0, r.read(cbuf, 0, cbuf.length));
-            System.out.println("got: " + v);
-            int i = Integer.parseInt(v);
-            i = i * i;
-            w.write(String.valueOf(i));
-            w.flush();
-            System.out.println("sent: " + i);
-            ns.close();
+            System.out.println("Listening on port " + PORT);
+            serverSocket = new ServerSocket(PORT);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Connection accepted from: " + socket);
+
+                Thread t = new ClientHandler(socket);
+                t.start();
+            }
         } catch (IOException e) {
-            System.err.println("i/o error: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.err.println("not an integer");
-        } 
+            System.err.println("I/O error: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
         new SqTCPServ();
+    }
+
+    private static class ClientHandler extends Thread {
+        private final Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                BufferedWriter w = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                BufferedReader r = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                char[] buf = new char[100];
+                String input = new String(buf, 0, r.read(buf, 0, buf.length));
+                System.out.println("Got: " + input);
+
+                try {
+                    int i = Integer.parseInt(input);
+                    i = i * i;
+                    w.write(String.valueOf(i));
+                    w.newLine();
+                    w.flush();
+                    System.out.println("Sent: " + i);
+                } catch (NumberFormatException e) {
+                    w.write("Invalid input, please send an integer.");
+                    w.newLine();
+                    w.flush();
+                    System.err.println("Not an integer");
+                }
+
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("I/O error: " + e.getMessage());
+            }
+        }
     }
 }
